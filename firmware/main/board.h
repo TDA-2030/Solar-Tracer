07709821a969c38aa0ae0ddbb22f62f3e5538ac3
zhouli <1932489836@qq.com>
2025-03-02 13:06:57 +0800

    fw: update

---
+++ firmware/main/board.h
@@ -0,0 +1,29 @@
+/*
+
+  */
+ #ifndef __BOARD_H_
+ #define __BOARD_H_
+ 
+ #ifdef __cplusplus
+ extern "C" {
+ #endif
+ 
+ #define BOARD_IO_MOTX_IN1 6
+ #define BOARD_IO_MOTX_IN2 6
+ #define BOARD_IO_MOTY_IN1 6
+ #define BOARD_IO_MOTY_IN2 6
+
+ #define BOARD_IO_GPS_RX 6
+ #define BOARD_IO_GPS_TX 6
+
+ #define BOARD_IO_IMU_TX 6
+ #define BOARD_IO_IMU_RX 6
+
+ 
+ 
+ #ifdef __cplusplus
+ }
+ #endif
+ 
+ #endif
+ 
